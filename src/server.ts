f07769e91929fbd3149ac056d658cb2520dce694
fabrigeas <fabrigeas@gmail.com>
2023-02-27 22:36:06 +0100

    feat: implement REST /api/users

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import App from './app';
 import OffersRoute from '@routes/offers.route';
+import UsersRoute from '@routes/users.route';
 
-const app = new App([new OffersRoute()]);
+const app = new App([new OffersRoute(), new UsersRoute()]);
 
 app.listen();
