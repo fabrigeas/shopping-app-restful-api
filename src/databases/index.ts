2bbbe7f6632bff14f1965d43eeacec37a7ad79dc
fabrigeas <fabrigeas@gmail.com>
2023-03-28 13:27:23 +0200

    build(frontend): update frontend bumdle

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,4 +1,4 @@
-const DB_HOST = process.env.NODE_ENV === 'local' ? 'mongo' : '127.0.0.1';
+const DB_HOST = process.env.NODE_ENV === 'production' ? 'mongo' : '127.0.0.1';
 
 export const dbConnection = {
   url: `mongodb://${DB_HOST}:27017/shop`,
